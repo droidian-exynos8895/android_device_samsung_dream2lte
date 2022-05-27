45e207286a5ebf28e5585aba99ff0cfbcf46cf4f
Parmjot Singh <parmjotsinghrobot@gmail.com>
2022-05-28 10:41:45 +1200

    Initial Ubuntu Touch stuff
    Change-Id: I8f8c6c279a1efe2e99cf3675287b574f71a02e55

--- lineage_dream2lte.mk
+++ lineage_dream2lte.mk
@@ -16,14 +16,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from dream2lte device
 $(call inherit-product, device/samsung/dream2lte/device.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halium stuff.
+$(call inherit-product, vendor/lineage/config/halium.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dream2lte
